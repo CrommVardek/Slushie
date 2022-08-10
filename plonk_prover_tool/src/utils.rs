3fa52f2a2f077123abf9fa9f5aaeea347f7c36ed
vasyl <123rrtyis2@gmail.com>
2022-08-10 16:50:07 +0300

    cli quick fix
    fix &[8 ; 1040] -> &[8]

--- plonk_prover_tool/src/utils.rs
+++ plonk_prover_tool/src/utils.rs
@@ -5,7 +5,7 @@ use std::fs::File;
 use std::io::prelude::*;
 use crate::DEFDIP;
 
-pub fn write_to_file(res: &[u8; 1040], path: &str) {
+pub fn write_to_file(res: &[u8], path: &str) {
     let mut f = File::create(path).expect("Unable to create file");
     f.write_all(res).expect("Unable to write data");
 }
@@ -28,13 +28,11 @@ pub fn input_json_in_array (json: Value) -> [[u8; 32]; DEFDIP] {
 pub fn json_parse(o: &str, file_path: &str) -> [[u8; 32]; DEFDIP] {
     if file_path != "test-json.json" {
         let json: serde_json::Value = serde_json::from_str(o).expect("JSON was not well-formatted");
-
         input_json_in_array (json)
     } else {
         let file = fs::File::open(file_path).expect("File should open read only");
         let json: serde_json::Value =
             serde_json::from_reader(file).expect("File should be proper JSON");
-
             input_json_in_array (json)
     }
 } 
