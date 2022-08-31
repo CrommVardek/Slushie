a4fdb5f1a6effdcbd5363b7529d9e4010c1eb555
pyaremenko <belachka228@icloud.com>
2022-09-01 02:00:28 +0300

    refactoring

--- jsonrpsee_server/src/main.rs
+++ jsonrpsee_server/src/main.rs
@@ -1,5 +1,6 @@
 pub mod keystore;
 pub mod methods;
+pub mod utils;
 
 use jsonrpsee::{
     core::{server::access_control::AccessControlBuilder, Error},
@@ -8,7 +9,8 @@ use jsonrpsee::{
 };
 use sp_keyring::AccountKeyring;
 use std::net::SocketAddr;
-use subxt::{ext::sp_core::bytes::from_hex, tx::PairSigner, PolkadotConfig};
+use subxt::ext::sp_core::bytes::from_hex;
+use subxt::{tx::PairSigner, PolkadotConfig};
 
 use crate::methods::withdraw;
 use shared::{public_inputs::*, public_types::*};
@@ -30,7 +32,13 @@ async fn main() -> anyhow::Result<()> {
             body: JSON.stringify({{
                 jsonrpc: '2.0',
                 method: 'withdraw',
-                params: [nullifier_hash, root, fee, recipient],
+                params: [nullifier_hash,
+                         root,
+                         proof,
+                         fee,
+                         recipient,
+                         relayer 
+                         ],
                 id: 1
             }})
         }}).then(res => {{
@@ -52,27 +60,38 @@ async fn setup_rpc_module() -> Result<RpcModule<()>, Error> {
     module.register_async_method("withdraw", |params, _| async move {
         let mut params_iter = params.parse::<Vec<String>>()?.into_iter();
 
-        let nullifier_hash: PoseidonHash = params_iter
-            .next()
-            .ok_or(CallError::InvalidParams(anyhow::Error::msg(
+        let nullifier_hash: PoseidonHash = from_hex(&params_iter.next().ok_or(
+            CallError::InvalidParams(anyhow::Error::msg(
                 "Nullifier Hash parameter is not provided.",
-            )))?
-            .as_bytes()
-            .try_into()
-            .map_err(|_| {
-                CallError::InvalidParams(anyhow::Error::msg("Invalid nullifier hash parameter."))
-            })?;
+            )),
+        )?)
+        .map_err(|_| {
+            CallError::InvalidParams(anyhow::Error::msg(
+                "Cannot decode nullifier hash parameter.",
+            ))
+        })?
+        .try_into()
+        .map_err(|_| {
+            CallError::InvalidParams(anyhow::Error::msg("Invalid nullifier hash parameter."))
+        })?;
 
-        let root = from_hex(&params_iter.next().ok_or(CallError::InvalidParams(
+        let root: PoseidonHash = from_hex(&params_iter.next().ok_or(CallError::InvalidParams(
             anyhow::Error::msg("Root parameter is not provided."),
         ))?)
-        .map_err(|_| CallError::InvalidParams(anyhow::Error::msg("Invalid root parameter.")))?
+        .map_err(|_| CallError::InvalidParams(anyhow::Error::msg("Cannot decode root parameter.")))?
         .try_into()
         .map_err(|_| CallError::InvalidParams(anyhow::Error::msg("Invalid root parameter.")))?;
 
-        let proof = [0; 1040];
+        let proof: SerializedProof = from_hex(&params_iter.next().ok_or(
+            CallError::InvalidParams(anyhow::Error::msg("Proof parameter is not provided.")),
+        )?)
+        .map_err(|_| {
+            CallError::InvalidParams(anyhow::Error::msg("Cannot decode proof parameter."))
+        })?
+        .try_into()
+        .map_err(|_| CallError::InvalidParams(anyhow::Error::msg("Invalid proof parameter.")))?;
 
-        let fee = params_iter
+        let fee: u64 = params_iter
             .next()
             .ok_or(CallError::InvalidParams(anyhow::Error::msg(
                 "Fee parameter is not provided.",
@@ -80,23 +99,29 @@ async fn setup_rpc_module() -> Result<RpcModule<()>, Error> {
             .parse::<u64>()
             .map_err(|_| CallError::InvalidParams(anyhow::Error::msg("Invalid fee parameter.")))?;
 
-        let recipient = params_iter
-            .next()
-            .ok_or(CallError::InvalidParams(anyhow::Error::msg(
-                "Recipient parameter is not provided.",
-            )))?
-            .as_bytes()
-            .try_into()
-            .map_err(|_| {
-                CallError::InvalidParams(anyhow::Error::msg("Invalid recipient parameter."))
-            })?;
+        let recipient: String =
+            params_iter
+                .next()
+                .ok_or(CallError::InvalidParams(anyhow::Error::msg(
+                    "Recipient parameter is not provided.",
+                )))?;
+
+        let relayer: String =
+            params_iter
+                .next()
+                .ok_or(CallError::InvalidParams(anyhow::Error::msg(
+                    "Relayer parameter is not provided.",
+                )))?;
+
         let inputs = WithdrawInputs {
             nullifier_hash,
             root,
             proof,
             fee,
             recipient,
+            relayer,
         };
+
         let signer: PairSigner<PolkadotConfig, sp_keyring::sr25519::sr25519::Pair> =
             PairSigner::new(AccountKeyring::Alice.pair());
         withdraw(signer, inputs)
@@ -135,6 +160,9 @@ mod tests {
     use jsonrpsee::core::client::ClientT;
     use jsonrpsee::http_client::HttpClientBuilder;
     use jsonrpsee::rpc_params;
+    use jsonrpsee::types::error::CallError;
+    use std::fs;
+    use std::str::from_utf8;
 
     #[tokio::test]
     async fn test_client() {
@@ -142,13 +170,15 @@ mod tests {
         let url = format!("http://{}", server_addr);
         let client = HttpClientBuilder::default().build(url).unwrap();
         let params = rpc_params!(
-            "aaaaaaaaaaaapaaaaaaaaaazaaaaaaaa",
-            "0x4ce946e968a0b477960eef24aafe0997350ba8f168ba2e4a546773556bdd1458",
+            "12E4700B2A16A02D2E5CAF0DD78F09B5162D221A952799E838A3B01BA4AB228C",
+            "ac1d8ce6455937aea84f526141df0967c8bec03c7f47928c5be75fbbe18910b5",
+            "81A146BD2DD8D5AFAE4DE592972226686321A63A589D268C1A5D90226339AB79DAEE6CA39DD61C4464A726637B96A39DB28DA37C949381AF3DEEFF5092E8A8E17CB63F639CEDFC803444CD1B6E29C0DBEE4800E3D362ABAB79563ECDF1BF0B08ACF3B31C770408D056691E1C0DD6217F9C35A66F3FDE31E542A4FC545A6384E1B6CECF977A77255E33995EDC409B06C6A1BEA66B71C2593D931856F59C256EE5C0E61DF1C60C16A7E713E0C4BB8B68EFE947DFD64783270B34274930768835488664290490349180003ECC5BB7EF014CA2C52ACB798BEACEF73B7C4CEFF0CAC0B9940E62DD76831D6BEC935655E3F7B685D0CCA9EF87691B06B316EDF4C3B15F8C2E90E447C0644CA224130EB4E1C3B53E3E7B495E590F646EC04F8496C9CA18AF951B79ABC937E25CA952F640604A0CACD6C4E6B74AA415FA36B062DF15B11A777D27EE5543958E5399B1619208D5188BCA955CF2158B67C782EA1EFBB303896162454E0BAFE2D35B9FBBF3FDC97104564A4EE142434D18CE0FB517F5DD73D0B2E6D9FBCDE70A9EC0A74B31A6A527EA7ABCB5BC7083849F7F32E83AABF534B5C55D2D7AE5AF6C0192D453BF706F807689E4DD67E82409A7881A93026920D7ED433FDE39C9EE12D981D3FCFA84E06E7BFECFA71302DB6AAC424E93A889B5AEBF8B899EB45008C67C68AF0F31BF25E48900D2BBD2B731994C92AEDAD463EF38A9AEC04A61B7CE571E2F2FCA9D4DFF1E98A8C011C9CEF920D5E7DBDCE66F1B0BBC7B403140785FE8669EEB2555A9773219D0FBE74E89341D5AFEE9FD08BB021C557EA0EE886396B03B0B4D700237416F2EEAF7722AA5B0642F50D7A50454334A7B8DDB028AEFAC2DA517A7A2665650844AAA7D131BB3563CEA31ACD6ACCE1F19D7087C45D0CA1B5E6FC760701D0C1637685CF22A54D9BBEFDC023D25EE62880E6E6A53F29C0BD785D24BB5B89707DBED1C7516BD65C5DF794BFF654FEB1FC5667E7F6FC48D25B1D573A473007AC57E363AEF89E0C2FA44BC7A429EB7F77A85406A400018C057441DA8F76E89B4B27E3D073CCCBF75AB2F81BF9EFC09F6B3B7120881A1301AF09AAA97C6EA26A863B9930E93113440180D088F3A58546E7EE49EC9769D34AB79C312C2290AC4F5C385431FCF0A249A381EFE5910FA4CCF24853FE4D450F5ADF1F7B81D29AAF2EC50FE734FA2FE46D566B38F085F40E0B21D253B81C35B7158BE42CBD464E2497EB2D3E43C968C47B5B9242B63F6AADFE09EE97FC4C155192EF0A689B38DAA1C659768A706AC423906986CE6D1160D49891B4A63C4361E1E50234252D13E800993DF7B2727C48A150222C2F4E76246804C642DA47F530C47FB2F80C738E1A110B531869F0A9C9D92DBCEE7830FD7DAB6E82AACFD27A77B9A1DC0D830E82CCC26CA27BC675686C6CACEA09830666AEF7F4A96D55FE99DBAB02E7AA56806669E1DED71379500",
             "1",
+            "5Gh8pDNFyir6ZdhkvNy2xGtfUNovRjxCzx5oMhhztXhGX3oZ",
             "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY"
         );
-        let response: Result<String, _> = client.request("withdraw", params).await;
 
+        let response: Result<String, _> = client.request("withdraw", params).await;
         assert_eq!("OK".to_string(), response.unwrap())
     }
 }
--- jsonrpsee_server/src/methods.rs
+++ jsonrpsee_server/src/methods.rs
@@ -1,12 +1,10 @@
+use crate::utils::proof_verification;
+use anyhow::Error;
 use async_once::AsyncOnce;
 use jsonrpsee::types::error::CallError;
 use lazy_static::lazy_static;
-use plonk_prover::{verify, Pubkey};
-use shared::{constants::DEFAULT_DEPTH, public_inputs::*, public_types::*};
-use std::fs::File;
-use std::io::Read;
-use std::str::from_utf8;
-use std::{fs, ops::Deref};
+use shared::public_inputs::*;
+use std::ops::Deref;
 use subxt::{
     ext::{
         sp_core::{blake2_256, H256},
@@ -27,23 +25,27 @@ pub mod node_runtime {}
 pub async fn withdraw(
     signer: PairSigner<PolkadotConfig, sp_keyring::sr25519::sr25519::Pair>,
     inputs: WithdrawInputs,
-) -> Result<H256, Box<dyn std::error::Error>> {
+) -> Result<H256, CallError> {
     let mut call_data = Vec::<u8>::new();
     call_data.append(&mut (&blake2_256("withdraw".as_bytes())[0..4]).to_vec());
     call_data.append(&mut scale::Encode::encode(&(
-        inputs.nullifier_hash,
-        inputs.root,
-        inputs.proof,
+        &inputs.nullifier_hash,
+        &inputs.root,
+        &inputs.proof,
         inputs.fee,
-        AccountId32::from_string(from_utf8(&inputs.recipient)?).unwrap(),
+        AccountId32::from_string(&inputs.recipient).map_err(|_| {
+            CallError::InvalidParams(anyhow::Error::msg("Invalid recipient address."))
+        })?,
     )));
 
     let tx = node_runtime::tx().contracts().call(
         MultiAddress::Id(
-            AccountId32::from_string("5E43Yg8EnG52PPy9J2K5ERVt9bXirn3NFPG5r7YABeaKZSZ9").unwrap(),
+            AccountId32::from_string("5FT7RMg7FfsmGZg2MnrdSgdTbvu4r7ZCYfZLPh8v3iEL2MCP").map_err(
+                |_| CallError::InvalidParams(anyhow::Error::msg("Invalid contract address.")),
+            )?,
         ),
         0,
-        900_000_000_000,
+        49_000_000_000_000,
         None,
         call_data,
     );
@@ -51,88 +53,29 @@ pub async fn withdraw(
         .tip(PlainTip::new(0))
         .era(Era::Immortal, API.get().await.deref().genesis_hash());
 
-    let tx_hash = API
-        .get()
-        .await
-        .deref()
-        .tx()
-        .sign_and_submit(&tx, &signer, tx_params)
-        .await?;
-    Ok(tx_hash)
-}
+    if proof_verification(&inputs).is_ok() {
+        let tx_hash = API
+            .get()
+            .await
+            .deref()
+            .tx()
+            .sign_and_submit(&tx, &signer, tx_params)
+            .await?;
+        return Ok(tx_hash);
+    }
 
-/// Proof verification.
-pub async fn proof_verification(
-    //Nullifier hash
-    h: String,
-    //Root
-    R: String,
-    //Recipient address
-    A: String,
-    //Relayer address
-    t: String,
-    //Fee
-    f: u64,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let public_parameters = include_bytes!("test-correct-pp");
-    let recipient: Pubkey = AccountId32::from_ss58check(&A)
-        .map_err(|_| {
-            CallError::InvalidParams(anyhow::Error::msg("Cannot decode recipient parameter."))
-        })?
-        .try_into()
-        .map_err(|_| {
-            CallError::InvalidParams(anyhow::Error::msg("Invalid recipient parameter."))
-        })?;
-
-    let relayer: Pubkey = AccountId32::from_ss58check(&t)
-        .map_err(|_| {
-            CallError::InvalidParams(anyhow::Error::msg("Cannot decode relayer parameter."))
-        })?
-        .try_into()
-        .map_err(|_| CallError::InvalidParams(anyhow::Error::msg("Invalid relayer parameter.")))?;
-
-    let root: [u8; 32] = hex::decode(R)
-        .map_err(|_| CallError::InvalidParams(anyhow::Error::msg("Cannot decode root parameter.")))?
-        .try_into()
-        .map_err(|_| CallError::InvalidParams(anyhow::Error::msg("Invalid root parameter.")))?;
-
-    let nullifier_hash: [u8; 32] = hex::decode(h)
-        .map_err(|_| {
-            CallError::InvalidParams(anyhow::Error::msg(
-                "Cannot decode nullifier hash parameter.",
-            ))
-        })?
-        .try_into()
-        .map_err(|_| {
-            CallError::InvalidParams(anyhow::Error::msg("Invalid nullifiier hash parameter."))
-        })?;
-
-    let proof: SerializedProof = fs::read("../plonk_prover_tool/test-proof")
-        .map_err(|_| {
-            CallError::InvalidParams(anyhow::Error::msg("Unable to read proof from a file."))
-        })?
-        .try_into()
-        .map_err(|_| CallError::InvalidParams(anyhow::Error::msg("Invalid proof.")))?;
-
-    verify::<DEFAULT_DEPTH>(
-        public_parameters,
-        nullifier_hash,
-        root,
-        recipient,
-        relayer,
-        f,
-        &proof,
-    )
-    .map_err(|_| CallError::InvalidParams(anyhow::Error::msg("Invalid proof.")))?;
-    Ok(())
+    return Err(CallError::Failed(anyhow::Error::msg(
+        "Transaction canceled due to invalid proof.",
+    )));
 }
 
 #[cfg(test)]
 
 mod tests {
-    use super::proof_verification;
     use crate::methods::node_runtime;
+    use crate::utils::proof_verification;
     use crate::withdraw;
+    use dusk_bytes::Serializable;
     use futures::StreamExt;
     use shared::public_inputs::*;
     use shared::public_types::*;
@@ -143,50 +86,25 @@ mod tests {
     use subxt::tx::PairSigner;
     use subxt::{OnlineClient, PolkadotConfig};
     #[tokio::test]
-    async fn test_proof_verification_correct() {
-        proof_verification(
-            "29B3E77C5F6A9178BDCA67EEB33F8518F3C3277782698DA356F0384BA8C3E959".to_string(),
-            "9f05ad0d662502e80ff0020a0184f4e3a74e1146fb7346694e49eb6cfeb01a8b".to_string(),
-            "5Gh8pDNFyir6ZdhkvNy2xGtfUNovRjxCzx5oMhhztXhGX3oZ".to_string(),
-            "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY".to_string(),
-            1,
-        )
-        .await
-        .unwrap();
-    }
-    #[tokio::test]
-    #[should_panic]
-    async fn test_proof_verification_error() {
-        proof_verification(
-            "1".to_string(),
-            "8ba12c81e85a2c538113cb85c42886a8ecf46fe8f64daf875f8c1609d7306883".to_string(),
-            "5ChyY5Rrn1ncJvUu77EpVDR6Ze74y1MT2ZSq6mjgMjbFgxda".to_string(),
-            "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY".to_string(),
-            1,
-        )
-        .await
-        .unwrap();
-    }
-    #[tokio::test]
     async fn test_withdraw() {
         let api = OnlineClient::<PolkadotConfig>::new().await.unwrap();
         let signer: PairSigner<PolkadotConfig, sp_keyring::sr25519::sr25519::Pair> =
             PairSigner::new(AccountKeyring::Alice.pair());
         let inputs = WithdrawInputs {
-            nullifier_hash: PoseidonHash::try_from("aaaaaaaaaaaiaaaaaaaaaaaaaaaaaaaz".as_bytes())
-                .unwrap(),
-            root: from_hex("0x4ce946e968a0b477960eef24aafe0997350ba8f168ba2e4a546773556bdd1458")
+            nullifier_hash: from_hex("12E4700B2A16A02D2E5CAF0DD78F09B5162D221A952799E838A3B01BA4AB228C").unwrap().try_into().unwrap(),
+            root: from_hex("e0d769fc156408415cc18bf731e665a60eb2c380fd5c615a347af6350f652a1d").unwrap().try_into().unwrap(),
+            proof: from_hex("ABB115036DB24FFFC2667645E0E2E113255BA93609321B51A09837DC4AA5E006A8CAE037C35B21904C5A7F39E0AFD2D095AC398A56B26A42E94709C928DF59AEBE481BF683651FA369E6D6AB167E60D20426419661DE73A521E7C6DD5211346E85C375F173FFA8016A18A442F0B32E38AD11598587A0EB4F6AF0A618F9BDB64C5BB30EB6FEF93A9447113CCD1DE5D5AEB3E2D47BB73FF5D44FAC13AE52B4AE21C036245BCC69B25F0D343837E7891E6AE079B54B74BFF8A0228AC81D4D9A75A995DFEF2CD84E417568D3587BC835761B93EC21A500A9DA96255DD1831BE68AA8E7C0077DBEB0801D4E64DC4CD6596D908EF0557F300F5A41E5FADCEC53BC9828B7AB77E3DA2C5CB33BADE91CEFBE362FF9EC7B0B89F43530BBBEA0BA9A279FD7A6F3F1ED0881D620C2EBE7F967173A8A57390BB34C04FA38F4B5002C3F038E2275ECFD582B5DC98DE0A73378EAED15B18FC7EA72087F1B8B150C88FDB8F0BE1425580887AD7E4A885897EBB70A0F3D51775B16A707134FC095F899923A6128BEA8E0E4097C742AF390AA83D56FC695B9A213191E81D1AE8A8F26E2398F68AE3323C726D363215E7128C8FB9A581706C9A67F456EAABD670C951AAACE22708ADA4F28A44B4510B5CA114F0BC430E3B29BE3E8EFDBFB3E07E1D49E3C36D0FE79A48484E41EF43AE39216B52F846A2321ACC58466C7613FEDBB79C149A8724A28408F476C53D5E9C54DA0DCD1C01AFA6C35A7135AF66BA19D5DEF749FF0C8729994012C171A73C5540CAEB91E63E4573E6E333E6A4057AA9DB26E4601CDFF012D902ED17D010BD7552D4FEADA093BE2BA4FE3A2B607E0D68DE3B374306BB68C677D5CF188E9C56AC540CADEDB052C67443F3A52B79BE4D73D44D74B37E3DD2224313B4716E66F8FA83FBCFD9C08E6DCB524D0551AA7AE237B1CDF1B8FB597E7CD517A93C90B40E7F836F7C74EEE1197EE24772BBBB8ED52894CF3DB5814DC4AD51D13A7F032463D69B444B00A3D66773C2BEDC91F83EA0E675A01277893CFBE18ACCCED8CD630CA2423295EDEA4C5BFD15071103CDB2F5BEDD543FE40B0E8E0EB6F137140A4A3A8728BB72CFE90565700469C12205315829BF6DAFEA679DAF32DECC35346E49918B4B8E094547011A35867713FF6EBA954DF8800D00DF850EB1CACD54199D50C117739F467490723EDC61018C247600A1ED39BCC2760073FD14B0EF46955B936443C72D2BEA66C70EC5938536B7AA2C05CF6E86C418294D5F3B6E76E606AF69429F11036A23902A79FD262E06BF81121E8FF8FE944AAC0E8C34FECA0BDBCF78094EAA1225FA01CFAEFAD0AEB09AA2A3486F34FE2D20187BC8C41E8A150F2CC7CB0472D7C8469EBD328D2480045B0F08F438FA0CC806A9AD62B47150BA150F5431F58D557A20E7FE0E6EB6AF5D46ED04BD868804910AFF7C203F1949FC15F45886CF73B46DAFB348CB4BF4C")
                 .unwrap()
                 .try_into()
                 .unwrap(),
-            proof: [0; 1040],
             fee: 1u64,
-            recipient: "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY"
-                .as_bytes()
-                .try_into()
-                .unwrap(),
+            recipient: "5Gh8pDNFyir6ZdhkvNy2xGtfUNovRjxCzx5oMhhztXhGX3oZ".to_string(),
+            relayer: "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY".to_string(),
         };
-        withdraw(signer, inputs).await.unwrap();
+
+        if proof_verification(&inputs).await.is_ok() {
+            withdraw(signer, inputs).await.unwrap();
+        }
 
         let mut events = api
             .events()
@@ -197,8 +115,6 @@ mod tests {
 
         let ev = events.next().await.unwrap();
         let details = ev.unwrap();
-        //How can we get all hashes in specific block&
-        //How can we check our extrinsic in block?
         assert_eq!(ApplyExtrinsic(1), details.phase);
     }
 }
